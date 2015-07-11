9c79b80b147b4b0725344b1b31d645fbb342a419
David Barratt <david@davidwbarratt.com>
2015-07-11 17:06:54 -0400

    Replace WordPress with Drupal

--- wp-content/themes/sailvenice/page.php
+++
@@ -1,5 +0,0 @@
-<?php get_header(); ?>
-<article>
-	<?php get_template_part( 'loop', 'page' ); ?>
-</article>
-<?php get_footer(); ?>
--- wp-content/themes/sailvenice/tag.php
+++
@@ -1,5 +0,0 @@
-<?php get_header(); ?>
-<article>
-<?php get_template_part( 'loop', 'tag' ); ?>
-</article>
-<?php get_footer(); ?>
