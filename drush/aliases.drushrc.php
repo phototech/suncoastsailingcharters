9ad39c0fdcb3af193a563d674debb4a88e2056b1
David Barratt <david.barratt@golfchannel.com>
2016-02-21 19:56:15 -0500

    Add drush aliases

---
+++ drush/aliases.drushrc.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @file
+ * Drush aliases.
+ */
+
+$aliases['local'] = [
+  'uri' => 'sailvenice.loc',
+];
+
+$aliases['dev'] = [
+  'root' => '/home/phototech/sites/sailvenice_dev/web',
+  'uri' => 'dev.sailvenice.com',
+  'remote-host' => 'origin.sailvenice.com',
+  'remote-user' => 'phototech',
+  'path-aliases' => [
+    '%drush-script' => '/home/phototech/sites/sailvenice_dev/vendor/bin/drush',
+  ],
+];
